0802fefbf3358679cb6c97bef21dcf3b6213c52e
Oskar Mikolajczyk <Oskar.Mikolajczyk@hyland.com>
2020-04-25 13:47:30 -0400

    feat(shopping-cart): complete shopping cart design

--- src/app/features/cart/cart/cart.component.ts
+++ src/app/features/cart/cart/cart.component.ts
@@ -12,83 +12,15 @@ import { CartItem } from 'app/core/models/cart-items.model';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CartComponent implements OnInit {
-  isLinear = true;
-  cartItems$ = this.facade.cartItems$.pipe(tap(cartItems => this.initForms(cartItems)));
-  overviewFormGroup: FormGroup;
-  infoFormGroup: FormGroup;
-  checkoutFormGroup: FormGroup;
-  confirmationFormGroup: FormGroup;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-  constructor(private breakpointObserver: BreakpointObserver, private fb: FormBuilder, private facade: FeaturesFacadeService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.breakpointObserver.observe([Breakpoints.Small]).subscribe(result => {
-      if (result.matches) {
-      } else {
-      }
-    });
-    this.firstFormGroup = this.fb.group({
-      firstCtrl: ['']
-    });
-    this.secondFormGroup = this.fb.group({
-      secondCtrl: ['']
-    });
-  }
+  ngOnInit(): void {}
 
-  private initForms(cartItems: CartItem[]): void {
-    this.initOverviewForm(cartItems);
-    this.initInfoForm();
-    this.initCheckoutForm();
-    this.initConfirmationForm();
-  }
+  onContinueShopping(): void {}
 
-  private initOverviewForm(cartItems: CartItem[]): void {
-    // have to be created based on what has been added to the cart
-    const cartItemsQuantitiesControls = cartItems.map(item => {
-      return this.fb.group({
-        id: item.id,
-        quantity: cartItems.reduce((total, x) => (x.cartItemId === item.cartItemId ? total + 1 : total), 0)
-      });
-    });
+  onUpdateCart(): void {}
 
-    this.overviewFormGroup = this.fb.group({
-      cartItems: this.fb.array(cartItemsQuantitiesControls)
-    });
-  }
-
-  private initInfoForm(): void {
-    this.infoFormGroup = this.fb.group({
-      customerInfo: this.fb.group({
-        firstName: this.fb.control('', Validators.required),
-        lastName: this.fb.control('', Validators.required),
-        email: this.fb.control('', [Validators.required, Validators.email]),
-        phone: this.fb.control('', Validators.pattern('[0-9]{0,10}'))
-      }),
-      shippingInfo: this.fb.array([this.createShippingInfo()]),
-      sameAsShipping: this.fb.control(true)
-    });
-  }
-
-  private createShippingInfo(): FormGroup {
-    return this.fb.group({
-      street: this.fb.control('', Validators.required),
-      city: this.fb.control('', Validators.required),
-      zipCode: this.fb.control('', Validators.required),
-      state: this.fb.control('', Validators.required)
-    });
-  }
-
-  private initCheckoutForm(): void {
-    this.checkoutFormGroup = this.fb.group({
-      cardInfo: this.fb.group({
-        number: this.fb.control('', Validators.required),
-        expDate: this.fb.control('', Validators.required),
-        cvc: this.fb.control('', Validators.required),
-        zip: this.fb.control('', Validators.required)
-      })
-    });
-  }
-
-  private initConfirmationForm(): void {}
+  onCheckOut(): void {}
 }
